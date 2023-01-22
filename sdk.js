93ccc72e46cf2cfba0b818ab865ba9fede28d6a2
Zakery Clarke <zakerytclarke@gmail.com>
2023-01-22 18:53:23 -0500

    Let's add some style

--- sdk.js
+++ sdk.js
@@ -1,5 +1,58 @@
 var teapot_wrapper = document.createElement("div");
 teapot_wrapper.innerHTML=`
+<style>
+#teapot-chat-wrapper{
+    width:80%;
+    border-radius:2vw;
+    background-color:#d2d4da;
+    text-align:center;
+    text-shadow:none;
+    min-width:600px;
+}
+#teapot-chat-wrapper input{
+    border-radius:1vw;
+    width:80%;
+    border:#ccc 1px;
+    font-size: 100%;
+    padding:3px;
+    padding-left:10px;
+    padding-right:10px;
+
+}
+
+#teapot-chat-header{
+    color:#2d3d3e;
+    font-size:200%;
+    text-align:center;
+    background-color:#f2f8fd; 
+    overflow:hidden;
+    border-radius:0.5vw;  
+}
+
+#teapot-chat-messages{
+ background-color:#f2f2f2;   
+ 
+ border-radius:0.5vw;  
+ 
+ padding:20px;
+}
+
+.message{
+    width:50%;
+    color:#fefefe;
+    border-radius:2vw;
+    padding:5px;
+    padding-left:10px;
+    padding-right:10px;
+}
+.messageUser{
+    background-color:#43CC47;
+    margin-left:45%
+}
+.messageBot{
+    background-color:#1982FC;
+}
+</style>
 <div id="teapot-chat-wrapper">
     <div id="teapot-chat-header">
         Realtor AI
@@ -9,7 +62,7 @@ teapot_wrapper.innerHTML=`
     <input id="teapot-chat-box" type="text"><button onclick="sendMessage()">Send</button>
 </div>
 `;
-document.getElementById("teapot").after(teapot_wrapper)
+document.getElementById("teapot").after(teapot_wrapper);
 
 var CONFIG = {
     client_id:'realtor'
@@ -48,7 +101,7 @@ function renderMessages(){
 function sendMessage(){
     var message = document.getElementById("teapot-chat-box").value;
     document.getElementById("teapot-chat-box").value="";
-    SESSION.chats.append({
+    SESSION.chats.push({
         'user':message
     })
 
