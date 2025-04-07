1d7697e8f2d37cb563b5462132f9da661413d699
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2025-04-07 15:01:52 -0400

    Update include.js

--- docs/include.js
+++ docs/include.js
@@ -43,4 +43,14 @@
 
   // Append the style to the head of the document
   document.head.appendChild(style);
+
+  (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:3333253,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
 })();
